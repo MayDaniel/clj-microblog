0e947102ed935d5d17fd45094d1aca1ffec85b77
MayDaniel <yamnad1@gmail.com>
2010-08-28 19:32:45 +0100

    Upgraded some versions.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :dependencies [[org.clojure/clojure "1.2.0"] 
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.2.5"]
-                 [hiccup "0.2.6"]
+                 [hiccup "0.3.0"]
                  [stupiddb "0.3.2"]
                  [clj-mail "0.1.0"]
-                 [clj-ini "0.1.1"]
+                 [clj-ini "0.1.2"]
                  [net.cgrand/moustache "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [ring/ring-devel "0.2.5"]])
